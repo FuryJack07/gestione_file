c522a3e7ff4c197df3a4e761c9a91ca0acaec48e
FuryJack07 <giacomo.quarena@gmail.com>
2026-02-23 13:09:48 +0100

    23_02_2026-2

--- connessioni/09_02_26/server.py
+++ connessioni/09_02_26/server.py
@@ -16,40 +16,50 @@ while True:
         #print("client: ",cl_add)
         #request = c_sock.recv(1024)
         #request = request.decode('utf-8')
-        request="P-test"
+        request="A-autore"
         response=""
-        f=open("biblioteca.txt", "r+")
-        linee=[]
+        f=open("biblioteca.txt", "r")
+        lines=[]
         for pos,line in enumerate(f):
-            linee.append(line.split(":"))
+            lines.append(line.split(":"))
         f.close()
         if request[0]=="T":
-            for linee_sing in linee:
-                if linee_sing[0] ==request[2:]:
-                    response="ci sono "+linee_sing[2]+" copie di "+request[2:]+" in libreria"
+            for lines_sing in lines:
+                if lines_sing[0] ==request[2:]:
+                    response="ci sono "+lines_sing[2]+" copie di "+request[2:]+" in libreria"
             if response=="":
                 response="titolo non presente"
         #fine richiesta T
         if request[0]=="A":
-            for linee_sing in linee:
-                if linee_sing[1]==request[2:]:
-                    response.append(linee_sing[1]+" ")
+            for lines_sing in lines:
+                if lines_sing[1]==request[2:]:
+                    response=response+lines_sing[0]+"; "
+            response=response.strip("; ")
             if response=="":
                 response="autore non presente"
             else:
-                response="autore "+request[2:]+"ha i seguenti libri in libreria "+response
+                response="autore "+request[2:]+" ha i seguenti libri in libreria: "+response
         #fine richiesta A
         if request[0]=="P":
-            for linee_sing in linee:
-                if linee_sing[0] ==request[2:]:
+            for lines_sing in lines:
+                if lines_sing[0] ==request[2:]:
+                    if int(lines_sing[2]>0):
+                        lines_sing[2]=str(int(lines_sing[2])-1)
+                        response="titolo prenotato. rimangono "+lines_sing[2]+"copie"
+                    else:
+                        response="titolo presente ma non disponibile"
+                    print(lines_sing)
                     print("A")
             if response=="":
                 response="titolo non presente"
             write_list=[]
-            for linee_sing in linee:
-                write_list.append("-".join(linee_sing))
+            for lines_sing in lines:
+                write_list.append(":".join(lines_sing))
             print(write_list)
-        
+            
+            f=open("biblioteca.txt", "w")
+            #scrivi nuove linee su file
+            f.close()
         print(response)
         #c_sock.send(response.encode('utf-8'))
     except:
